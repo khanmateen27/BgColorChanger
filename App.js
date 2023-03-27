e2b08d365b3f71656591ab95c80e98be25e4d999
khanmateen27 <khanmateen2712@gmail.com>
2023-03-27 20:15:16 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { View , Text, Button , StyleSheet , Pressable } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = ()=>{
+
+  const [BgColor , setBgColor] = useState(`rgb(125 , 0 , 255)`);
+
+  const onPressHandler = ()=>{
+    let randomColor = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
+    setBgColor(randomColor);
+  }
+
+  const ResetHandler = ()=>{
+    setBgColor(`rgb(0,0,0)`);
+  }
+
+  return <View style={[styles.container , {backgroundColor : BgColor}]}>
+    <Text style={{
+      padding : 10 ,
+      fontSize : 20 ,
+      color : "white"
+    }}>Background Color Changer</Text>
+    <Button title="CLick Me" onPress={onPressHandler} />
+    <Pressable onPress={ResetHandler}><Text style={styles.btn}>RESET</Text></Pressable>
+  </View>
+
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container : {
+    flex:1 ,
+    justifyContent : "center" ,
+    alignItems : "center" ,
+    backgroundColor:'rgb(125 , 0 , 255)'
+  } ,
+  btn : {
+    margin : 5 ,
+    padding : 10 ,
+    backgroundColor : "black" ,
+    color : "white"
+  }
+})
